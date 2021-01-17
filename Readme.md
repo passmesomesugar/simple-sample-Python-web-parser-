d15990f870bde6ecc3e52ab80bf1b4f5b78d0247
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-01-17 13:15:40 +0600

    'get_html_page' added

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
-## Installation
+## Description 
 
 Simple example Python parser for this [site](https://astana.gov.kz/). Collects news having 'covid' title.
 
--- main.py
+++ main.py
@@ -2,21 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-HOST = 'https://astana.gov.kz'
+HOST = 'http://astana.gov.kz'
 URL = 'http://astana.gov.kz/ru/search?query=covid'
 HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
 }
+news_list = []
+
 
 # PAGINATION = input("Enter number of pages to be parsed, Введите количество страниц для парсинга")
 # PAGINATION = int(PAGINATION.strip())
-req = requests.get(URL)
+
+def get_html_page(url, params=''):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+
+# req = requests.get(URL)
+req = get_html_page(URL)
 soup = BeautifulSoup(req.content, 'html.parser')
 # Лямбда что бы ограничить результаты только классом 'result' который содержит заголовки.
 # В противном случае результатов будет слишком много.
 news_items = soup.find_all(lambda tag: tag.name == 'div' and tag.get('class') == ['result'])
-news_list = []
 
 for news_item in news_items:
     news_list.append(
@@ -26,7 +34,7 @@ for news_item in news_items:
             'date': news_item.find(class_='result-date').get_text()
         }
     )
-
+# Информация попадает в лист со словарями.
 keys = news_list[0].keys()
 with open('parse_data.csv', 'w', newline='') as output_file:
     dict_writer = csv.DictWriter(output_file, keys)
